6dd1005de5fe0432702b75c26312453b2b4a5692
toejamow <80569639+toejamow@users.noreply.github.com>
2024-04-05 13:00:06 +0500

    замена ссылок на иконку

--- connect/header.php
+++ connect/header.php
@@ -1,5 +1,6 @@
 <nav>
-        <a href="registration.php">Зарегистрироваться</a>
-        <a id="logo-link" href="index.php"><img class="logo" src="images/logo.png" alt="Лого"></a>
-        <a href="authorization.php">Войти</a>
-    </nav>
+    <a id="logo-link" href="../index.php"><img class="logo" src="../images/logo.png" alt="Лого"></a>
+    <div class="reg-auth-links">
+    <a href="../reg-auth/authorization.php"><img id="login" src="../images/login.png" alt="Войти"></a>
+    </div>
+</nav>
