642a34b88d494090efa33ea31cfe88fa70a5a4f1
toejamow <80569639+toejamow@users.noreply.github.com>
2024-04-05 12:59:50 +0500

    исправление ошибок при верстке списков

--- connect/footer.php
+++ connect/footer.php
@@ -1,41 +1,25 @@
 <footer>
-        <div class="links">
-            <h3>Контакты</h3>
-            <ul>
-                <a href="#">
-                    <li>Smth</li>
-                </a>
-                <a href="#">
-                    <li>Smth</li>
-                </a>
-                <a href="#">
-                    <li>Smth</li>
-                </a>
-                <a href="#">
-                    <li>Smth</li>
-                </a>
-            </ul>
-        </div>
+    <div class="links">
+        <h3>Контакты</h3>
+        <ul>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">О нас</a></li>
+        </ul>
+    </div>
 
-        <div class="logo-cont">
-            <img class="logo" src="images/logo.png" alt="Лого">
-            <p>FIZZ (c) 2024 All rights reserved.</p>
-        </div>
-        <div class="links">
-            <h3>Соц. сети</h3>
-            <ul>
-                <a href="#">
-                    <li>Smth</li>
-                </a>
-                <a href="#">
-                    <li>Smth</li>
-                </a>
-                <a href="#">
-                    <li>Smth</li>
-                </a>
-                <a href="#">
-                    <li>Smth</li>
-                </a>
-            </ul>
-        </div>
-    </footer>
+    <div class="logo-cont">
+        <img class="logo" src="../images/logo.png" alt="Лого">
+        <p>FIZZ (c) 2024 All rights reserved.</p>
+    </div>
+    <div class="links">
+        <h3>Соц. сети</h3>
+        <ul>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">О нас</a></li>
+        </ul>
+    </div>
+</footer>
