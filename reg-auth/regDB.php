66ecb82c541ed63f18afaf43d94ed3ee8a6d0d28
toejamow <80569639+toejamow@users.noreply.github.com>
2024-04-06 11:52:21 +0500

    исправление путей + переадресация при неудачной авториз.

--- reg-auth/regDB.php
+++ reg-auth/regDB.php
@@ -8,13 +8,12 @@ $pass = strip_tags(trim($_POST['pass']));
 $result = mysqli_query($con,"SELECT * FROM `Users` WHERE `Email` = '$email'");
 $user = mysqli_fetch_assoc($result); // Конвертируем в массив
 if(!empty($user)){
-	echo "Данная эл. почта уже используется!";
-	exit();
+	echo "<script>alert('Данная эл. почта уже используется!'); location.href = 'registration.php';</script>";
 } else {
     mysqli_query($con,"INSERT INTO `Users`(`Email`, `Password_hash`, `Bonus_points`) VALUES ('$email','$pass','0')");
     echo "<script>alert('Регистрация прошла успешно!')</script>";
     $_SESSION["user_id"] = mysqli_insert_id($con);
-    header('Location:index.php');
+    header('Location:../user');
 }
 
 
