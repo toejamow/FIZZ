c26c4fa08c137f3c01f67d78ef59a48911f5a2d3
toejamow <80569639+toejamow@users.noreply.github.com>
2024-04-15 00:30:08 +0500

    Update authDB.php

--- reg-auth/authDB.php
+++ reg-auth/authDB.php
@@ -10,7 +10,7 @@ $user = mysqli_fetch_assoc(mysqli_query($con, "SELECT * FROM `Users` WHERE `emai
 if(count($user) == 0 ) {
     echo "<script>alert('Проверьте корректность введенность данных!');</script>";
 } else {
-    $_SESSION["user_id"] = $user["user_id"];
+    $_SESSION["user_id"] = $user["User_id"];
 	header('Location: ../admin');
 }
 
