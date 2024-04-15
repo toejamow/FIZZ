4248af412ae2874f2e164d3ea046bbb6882749f1
toejamow <80569639+toejamow@users.noreply.github.com>
2024-04-15 21:59:23 +0500

    разработка функции добавления продукта

--- admin/forms/prodADM.php
+++ admin/forms/prodADM.php
@@ -2,5 +2,15 @@
 
 include ("../../connect/connect.php");
 
+$name = isset($_POST["name"])?$_POST["name"]:false;
+$descr = isset($_POST["descr"])?$_POST["descr"]:false;
+$categ = isset($_POST["categ"])?$_POST["categ"]:false;
+$price = isset($_POST["price"])?$_POST["price"]:false;
+$image = isset($_FILES["image"]["tmp_name"])?$_FILES["image"]:false;
+
+if ($name) {
+    $image_name = $image["name"];
+    $result = mysqli_query($con, "INSERT INTO `Product`(`Name`, `Description`, `Category_id`, `Price`, `Image`) VALUES ('$name','$descr','$categ', $price ,'$image_name')");
+}
 
 ?>
--- admin/product-adm.php
+++ admin/product-adm.php
@@ -22,10 +22,30 @@
 
         <h2>Продукты</h2>
 
-            <form action="forms/prodADM.php">
-                <label for=""></label>
-                <input type="text">
+        
+        <h3>Продукт №ID</h3>
+            <form action="forms/prodADM.php" method="POST" enctype="multipart/form-data">
+                <label for="name">Название</label>
+                <input id="name" name="name" type="text">
+
+                <label for="descr">Описание</label>
+                <input id="descr" name="descr" type="text">
+
+                <label for="categ">Категория</label>
+                <select name="categ" id="categ">
+                    <option value="1">1</option>
+                </select>
+
+                <label for="price">Цена</label>
+                <input id="price" name="price" type="text">
+
+                <label for="image">Изображение</label>
+                <input type="file" id="image" name="image" accept="image/*" >
+
+                <input type="submit" value="Сохранить">
+
             </form>
+
         </section>
     </main>
 
